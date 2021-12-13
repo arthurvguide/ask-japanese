594b9c70369863bdca5693c02fc7f6f2f3d562f2
arthurvguide <arthurvguideuk@gmail.com>
2021-12-13 23:55:30 +0000

    Create Table Model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Table(models.Model):
+    size = models.IntegerField()
+
+    class Meta:
+        ordering = ["-size"]
+
+    def __str__(self):
+        return str(self.size)
--- static/js/script.js
+++ static/js/script.js
@@ -104,4 +104,3 @@ desserts.addEventListener("click", () => {
         }
       };
 });
-
