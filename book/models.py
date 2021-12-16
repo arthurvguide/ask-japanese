191d8974c67b77f73105316c8e408d5ee8132ab4
arthurvguide <arthurvguideuk@gmail.com>
2021-12-16 00:01:59 +0000

    Add TIME_CHOICES, edit booking model

--- book/models.py
+++ book/models.py
@@ -1,6 +1,33 @@
+from datetime import datetime
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+TIME_CHOICES = (
+    (0, '11:00'),
+    (1, '11:30'),
+    (2, '12:00'),
+    (3, '12:30'),
+    (4, '13:00'),
+    (5, '13:30'),
+    (6, '14:00'),
+    (7, '14:30'),
+    (8, '15:00'),
+    (9, '15:30'),
+    (10, '16:00'),
+    (11, '16:30'),
+    (12, '17:00'),
+    (13, '17:30'),
+    (14, '18:00'),
+    (15, '18:30'),
+    (16, '19:00'),
+    (17, '19:30'),
+    (18, '20:00'),
+    (19, '20:30'),
+    (20, '21:00'),
+    (21, '21:30'), 
+)
 
 
 class Table(models.Model):
@@ -18,6 +45,7 @@ class Table(models.Model):
     def __str__(self):
         return str(self.size)
 
+
 class Booking(models.Model):
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=50)
@@ -28,13 +56,13 @@ class Booking(models.Model):
             MinValueValidator(1)
         ]
      )
-    booking_date_time_start = models.DateTimeField()
-    booking_date_time_end = models.DateTimeField()
-    phone_number = PhoneNumberField(unique = True, null = False, blank = False)
-    email = models.EmailField()
+    booking_date = models.DateField(default=datetime.now, blank=False)
+    booking_time = models.CharField(default=TIME_CHOICES[21],
+        max_length=30, choices=TIME_CHOICES)
+    phone_number = PhoneNumberField(unique=True, null=False, blank=False)
     
     class Meta:
-        ordering = ["booking_date_time_start"]
+        ordering = ["booking_date"]
 
     def __str__(self):
         return self.full_name
