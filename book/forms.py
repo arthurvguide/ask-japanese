548d8e00b6a3fd368b6176bc7894bab6cc6bc0ba
arthurvguide <arthurvguideuk@gmail.com>
2021-12-21 00:56:13 +0000

    Edit Booking form

--- book/forms.py
+++ book/forms.py
@@ -2,18 +2,12 @@ from .models import Booking
 from django import forms
 
 
-class DateInput(forms.DateInput):
-    input_type = 'date'
-
-
 class BookingForm(forms.ModelForm):
  
     class Meta:
         model = Booking  
-        labels = {
-        'full_name':'name',
-        'booking_date': 'date',
-        'booking_time': 'time'
-        }
-        fields = ('full_name', 'party_size','booking_date', 'booking_time', 'phone_number')
-        widgets = {'booking_date': DateInput()}
+        labels = {'full_name': 'name',
+                  'booking_date_start': 'when'}
+
+        fields = ('full_name', 'party_size',
+                  'booking_date_start', 'phone_number')
