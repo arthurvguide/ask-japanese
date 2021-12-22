80fc9b5816f83e2932739dadaf1d7ac9451fe5b8
arthurvguide <arthurvguideuk@gmail.com>
2021-12-22 02:43:41 +0000

    remove labels from bookingform

--- book/forms.py
+++ book/forms.py
@@ -6,8 +6,11 @@ class BookingForm(forms.ModelForm):
  
     class Meta:
         model = Booking  
-        labels = {'full_name': 'name',
-                  'booking_date_start': 'when'}
+        labels = {'full_name': 'Full Name',
+                  'booking_date_start': 'When',
+                  'phone_number': 'Contact Number',
+                  'party_size': 'Group Size'}
 
         fields = ('full_name', 'party_size',
                   'booking_date_start', 'phone_number')
+
