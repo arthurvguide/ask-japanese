d47b616e725d8356e8b937aedb218db77bad0824
arthurvguide <arthurvguideuk@gmail.com>
2022-04-29 09:15:59 +0000

    Style logout page

---
+++ static/js/accounts/logout.js
@@ -0,0 +1,7 @@
+// Script to customize the standard allauth logout page
+const logoutBtn = document.getElementsByTagName("button")[1];
+
+window.onload = () => {
+
+    logoutBtn.classList.add("btn");
+};
