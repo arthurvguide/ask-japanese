988c0a874764d6ccc8c1673219c498586b1a8df9
arthurvguide <arthurvguideuk@gmail.com>
2022-04-29 10:38:05 +0000

    fix accounts script/css/html

--- static/js/accounts/logout.js
+++
@@ -1,7 +0,0 @@
-// Script to customize the standard allauth logout page
-const logoutBtn = document.getElementsByTagName("button")[1];
-
-window.onload = () => {
-
-    logoutBtn.classList.add("btn");
-};
